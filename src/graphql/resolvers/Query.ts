a63acbc05d639501c558f1f4d4806fd157b6a68b
Corbin Murray <corbin.murray63@yahoo.com>
2021-03-20 14:49:27 -0500

    Update files for TPR

--- src/graphql/resolvers/Query.ts
+++ src/graphql/resolvers/Query.ts
@@ -7,8 +7,11 @@ const getAllDataHandler = async (parent, args, context, info) => {
     where: { device_eui: device_eui },
   });
 
-  console.log(deviceUpModels)
+  return deviceUpModels.map(model => model.toJSON());
 };
+
+
+// For the date range query we need to verify how ChirpStack saves the dates (locale vs UTC)
 
 const Query = {
   getAllData: getAllDataHandler,
