d03b4e83a89bd375a58d56440d49d4e4ff24a49d
Corbin Murray <corbin.murray63@yahoo.com>
2021-04-26 17:29:23 -0500

    Change to custom header

--- src/configs/authorization.config.ts
+++ src/configs/authorization.config.ts
@@ -1,13 +1,9 @@
 import { API_KEY } from "./tempApiKey.config";
 
-const authKey = "authorization-key";
+const authKey = "x-auth-key";
 
 export const authorizationMiddleware = (req, res, next) => {
-  if (!req.headers[authKey]) {
-    return res.sendStatus(401);
-  }
-
-  if (req.headers[authKey] !== API_KEY) {
+  if (!req.headers[authKey] || req.headers[authKey] !== API_KEY) {
     return res.sendStatus(401);
   }
 
